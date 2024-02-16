80a1b8070098cd53b1850d1cbaee489b3f272a47
Sergey Koksharov <gentoosiastic@gmail.com>
2024-02-16 10:31:29 +0300

    fix: fix some minor problems in configs

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 
 const baseConfig = {
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   mode: 'development',
   module: {
     rules: [
@@ -20,12 +20,12 @@ const baseConfig = {
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new DotenvWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
+      template: path.resolve(__dirname, 'src', 'index.html'),
       filename: 'index.html',
     }),
     new CleanWebpackPlugin(),
