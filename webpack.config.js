15a8b6d9de45cf5b49ee33274cc81af6ac23b5af
DaryaAniskevich <darya.aniskevich@mail.ru>
2024-02-26 00:12:03 +0300

    init: ts, eslint, webpack dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,15 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+              },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
         filename: 'index.js',
